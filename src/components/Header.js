06a0dc7556f8fa46c1a93451e4aa3bcfa15db7aa
kaido12 <kaidofalcon@gmail.com>
2022-10-15 23:12:02 +0530

    Authetication done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import Image from 'next/image';
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function Header() {
+
+    const {data: session} = useSession();
+    const router = useRouter();
+
   return (
     <header>
     {/* Top Navbar */}
         <div className='flex items-center bg-amazon_blue p-1 py-2 flex-grow'>
             <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-                <Image 
+                <Image
+                    onClick={() => router.push('/')}
                     src='https://links.papareact.com/f90'
                     width={150}
                     height={40}
@@ -24,15 +30,17 @@ function Header() {
 
             {/* Right Section */}
             <div className='text-white text-xs space-x-6 mx-6 flex items-center whitespace-nowrap'>
-                <div onClick={signIn} className='cursor-pointer link'>
-                    <p className=''>Hello Kaido</p>
+                <div onClick={!session ? signIn : signOut} className='cursor-pointer link'>
+                    <p className=''>
+                        {session ? `Hello, ${session.user.name}` : 'Sign In'}
+                    </p>
                     <p className='font-extrabold md:text-sm'>Account & Lists</p>
                 </div>
                 <div className='link'>
                     <p>Returns</p>
                     <p className='font-extrabold md:text-sm'>Orders</p>
                 </div>
-                <div className='link relative flex items-center'>
+                <div onClick={() => router.push('/checkout')} className='link relative flex items-center'>
                     <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
                     <ShoppingCartIcon className='h-10' />
                     <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
