ae192c5421e2cb4a402fab91e50ed09867002f54
kaido12 <kaidofalcon@gmail.com>
2023-04-19 20:54:34 +0530

    commit

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,7 +3,7 @@ import Product from './Product';
 
 function ProductFeed({ products }) {
   return (
-    <div className='grid grid-flow-row-dense sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-40 mx-auto'>
+    <div className='grid grid-flow-row-dense sm:grid-cols-1 md:grid-cols-2 md:-mt-40 mx-auto'>
         {products.slice(0,4).map(({id , title, price, description, image, category}) => (
         <Product 
             key={id}
@@ -68,7 +68,7 @@ function ProductFeed({ products }) {
             className='md:col-span-full'    
         />
 
-        <div className='md:grid-cols-4'>
+        <div className='md:grid-col-2'>
             {products.slice(13,14).map(({id , title, price, description, image, category}) => (
             <Product 
                 key={id}
